76ab8832cf898bbd0f8f0be92d49916e5da739a5
JadenSWang <jaden.shiteng.wang@gmail.com>
2020-01-08 11:56:20 -0500

    Replaced Modals

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -2,38 +2,75 @@ import React, { Component } from "react";
 
 class Blog extends Component {
   state = {
-    numbers: [0, 1, 2, 3, 4],
     articles: [
       [["Ducks are slowly taking over the world"], ["A brand new "]],
-      [""],
-      [""],
-      [""],
-      [""]
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]],
+      [["Ducks are slowly taking over the world"], ["A brand new "]]
     ]
   };
-
   render() {
-    var listItems = numbers.map(i => (
-      <div class="blogPreview">
-        <li key={state.numbers[i].toString()}>{state.numbers[i]}</li>
-        <div class="col s12 m4">
-          <div class="z-depth-0">
-            <div class="card hoverable waves-effect waves-block waves-light">
-              <div class="card-image">
-                <img src="/pictures/duck.jpg" alt="" />
-                <span class="card-title">Card Title</span>
-              </div>
-              <div class="card-content">
-                <p>{state.description[i]}</p>
-              </div>
+    return (
+      <React.Fragment>
+        <div class="row">{loadPreviews(this.state.articles)}</div>
+      </React.Fragment>
+    );
+  }
+}
+
+function loadPreviews(articles) {
+  const elements = [];
+
+  for (var i = 0; i < articles.length; i++) {
+    elements.push(
+      LoadPreview({ title: articles[i][0], description: articles[i][1] })
+    );
+  }
+
+  return elements;
+}
+
+function LoadPreview(props) {
+  return (
+    <div class="blogPreview">
+      <div class="col s12 m4">
+        <div class="z-depth-0">
+          <div class="card hoverable waves-effect waves-block waves-light">
+            <div class="card-image">
+              <img src="/pictures/duck.jpg" alt="" />
+              <span class="card-title">{props.title}</span>
+            </div>
+            <div class="card-content">
+              <p>{props.description}</p>
             </div>
           </div>
         </div>
       </div>
-    ));
-
-    return <div class="row">{listItems}</div>;
-  }
+    </div>
+  );
 }
 
 export default Blog;
