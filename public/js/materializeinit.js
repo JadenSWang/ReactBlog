46d16406d9637d9ea86bc8d83d96db2502b01393
JadenSWang <jaden.shiteng.wang@gmail.com>
2020-01-27 14:25:41 -0500

    Moved react init to a separate file

---
+++ public/js/materializeinit.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$(".fixed-action-btn").click(function() {
+		$("#nav-icon3").toggleClass("open");
+	});
+});
+
+// Floating Action Button
+document.addEventListener("DOMContentLoaded", function() {
+	// floating action buttons
+	var elems_fab = document.querySelectorAll(".fixed-action-btn");
+	M.FloatingActionButton.init(elems_fab, { hoverEnabled: false });
+
+	// tooltips
+	var elems_tooltip = document.querySelectorAll(".tooltipped");
+	M.Tooltip.init(elems_tooltip, {});
+
+	var elems_modal = document.querySelectorAll(".modal");
+	var instances = M.Modal.init(elems_modal, {});
+});
