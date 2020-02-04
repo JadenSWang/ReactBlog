0246c3bb88e33a288ae4c79368cf6756e5bd668a
JadenSWang <jaden.shiteng.wang@gmail.com>
2020-02-03 20:06:09 -0500

    Understand Backend Now

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var router = express.Router();
+var path = require("path");
 
 router.get("/", (req, res, next) => {
-	res.render();
+	res.sendFile("index.html", { rootj: path.join(__dirname, "../") });
 });
 
 module.exports = router;
